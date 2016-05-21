01c8946c9276e94d934c4ae869e0eeeb8a041db8
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2016-05-21 15:31:20 +0200

    real data reloading

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
-var data = {
-  title: 1,
-  description: "“Senior security engineer on his way to deliver comments to developers”",
-  image_url: "http://67.media.tumblr.com/fa1ba165f49884d3339f156ca6ed5493/tumblr_o5ztxr9ZRr1ugyavxo1_1280.jpg",
-};
-
-$.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://classicprogrammerpaintings.com/random') + '&callback=?', function(data){
-  var el = $( '<div></div>' );
-  el.html(data.contents);
-  image_url = $('section#posts .photo-wrapper img', el)[0].src;
-  infos = _.map($('section#posts figcaption.caption p', el), "textContent");
-
-  console.log({
-    image_url: image_url,
-    title: infos[0],
-    description: _.tail(infos).join(', '),
-  });
-});
-
 new Vue({
   el: '#app',
   data: {
-    title: data.title,
-    description: data.description,
-    image_url: data.image_url,
+    title: "“Last sprint before the deadline”",
+    description: "Józef Chełmoński, 1881, Oil on Canvas",
+    image_url: "http://65.media.tumblr.com/868169650b9c0520b3ccf88a96b9e9de/tumblr_o632v3uxOW1ugyavxo1_1280.jpg",
+  },
+  methods: {
+    reloadPainting: function () {
+      that = this;
+      $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://classicprogrammerpaintings.com/random') + '&callback=?', function(data){
+        var el = $( '<div></div>' );
+        el.html(data.contents);
+        infos = _.map($('section#posts figcaption.caption p', el), "textContent");
+        that.image_url = $('section#posts .photo-wrapper img', el)[0].src;
+        that.title = infos[0];
+        that.description = _.tail(infos).join(', ');
+      });
+    }
+  },
+  ready: function () {
+    that = this;
+    setInterval(function () {
+      that.reloadPainting();
+    }, 5000);
   }
-})
+});
